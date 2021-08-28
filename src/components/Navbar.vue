04a5bf7e5a492defe284c8bb5caf289bea30a828
Srijan Das <srijandas18.sd@gmail.com>
2021-08-28 16:20:25 +0530

    navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="navbar">
+    <div>
+      <router-link to="/" class="navbar__brand"
+        ><strong> {{ brandName }} </strong></router-link
+      >
+    </div>
+    <div class="navbar__rightItems">
+      <div class="cart">
+        <router-link to="/cart" class="button is-success">
+          <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+          <span>Cart ({{ cartTotalLength }})</span>
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  props: ["brandName"],
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  background-color: #363636;
+  display: flex;
+  align-items: center;
+  padding: 10px;
+}
+.navbar__brand {
+  color: white;
+}
+</style>
